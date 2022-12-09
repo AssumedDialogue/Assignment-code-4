a10ba0fcfc06f6d655b9ef53cf7e628f61407a59
AssumedDialogue <114254777+AssumedDialogue@users.noreply.github.com>
2022-12-09 13:26:33 -0500

    created json file

--- Main.py
+++ Main.py
@@ -5,6 +5,12 @@
 ################
 
 import json
+
+class Librabry:
+    def __init__(self, BookName, BookID, BookAuthor):
+        self.name = BookName
+        self.Id = BookID
+        self.Author = BookAuthor
  
 class create:
     def bleh():
@@ -23,24 +29,27 @@ class edit():
         print()
 
 print("Welcome to the Library!" + '\n' + "What would you like to do?" )
-while:
-    try:
-        MC = input( '\n' + '\n'+ "1. Search up a Book " + '\n' + "2. Add a new book " + '\n' + "3. Delete a book " + '\n' + "4. Edit a books information") 
-    except:
-        print("Enter a valid options") 
-
 while True:
     try:
-        BookName = input("What is the title of your book?: ")
-        BookID = input("What is the ID Number of your book? (Must be 8 digits): ")
-        BookAuthor = input("What is the Authors name of your book?: ")
-        if MC == "1":
-            Book = read(BookName,BookID, BookAuthor)
-        elif MC == "2":
-            Book = create(BookName,BookID, BookAuthor)
-        elif MC == "3":
-            Book = delete(BookName,BookID, BookAuthor)
-        elif MC == "4":
-            Book = edit(BookName,BookID, BookAuthor)
-    except:
-        print("Sorry you did not enter the correct information")
+        NN = ("1","2","3","4")
+        MC = input( '\n' + "1. Search up a Book " + '\n' + "2. Add a new book " + '\n' + "3. Delete a book " + '\n' + "4. Edit a books information")
+        if MC in NN:
+            raise MyException
+    except MyException:
+        print("Enter a valid option")
+        break
+        
+    
+
+BookName = input("What is the title of your book?: ")
+BookID = input("What is the ID Number of your book? (Must be 8 digits): ")
+BookAuthor = input("What is the Authors name of your book?: ")
+if MC == "1":
+    Book = read(BookName,BookID, BookAuthor)
+elif MC == "2":
+    Book = create(BookName,BookID, BookAuthor)
+elif MC == "3":
+    Book = delete(BookName,BookID, BookAuthor)
+elif MC == "4":
+    Book = edit(BookName,BookID, BookAuthor)
+
