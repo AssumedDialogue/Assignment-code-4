f0311c52958115a60354505a2450ed1dbcbc5793
AssumedDialogue <114254777+AssumedDialogue@users.noreply.github.com>
2022-12-11 00:54:57 -0500

    .....

--- Main.py
+++ Main.py
@@ -5,8 +5,10 @@
 ################
 
 import json
- 
-f=open(Areeba.json)
+try:
+    Areeba = (r"C:\Users\minha\Assignment code 4\Areeba.json")
+except FileNotFoundError:
+    print('File not available')
  
 
 def Create():
@@ -17,7 +19,7 @@ def Create():
     NewBook["Title"] = input("Enter New Book title: ")
     if NewBook["Title"] not in Areeba.keys():
         NewBook["Author"] = input("Enter the Author of the book: ")
-        NewBook["BookId"] = input("Enter the book ID number: ") #WHILE LOOPP
+        NewBook["BookId"] = input("Enter the book ID number: ") 
         NewBook["Genre"] = input("Enter the Genre of book: ")
         temp.append(NewBook)
         with open(Areeba,'w') as f:
@@ -27,21 +29,19 @@ def Create():
 def Delete():
     with open(Areeba.json,'r') as f:
         temp = json.load(f)
-    print(temp)
-    UserBook=[]
-    UserBook["Title"] = input("Enter the Book title you want to delete: ")
-    print("Enter The title of The book Which You Want To Delete :- ")
-    temp = input()
-    if temp in Areeba.keys():
-        Areeba.pop(temp)  # here we are removing that particular data
-        print("Book "+str(temp)+" iss Deleted")
-    else:
-        print("Invalid Book")
+    temp = input("Enter The info you'd like to delete: ")
+    while True:
+        if temp in Areeba.keys():
+            Areeba.pop(temp)
+            print("Book info "+str(temp)+" is Deleted")
+            break
+        else:
+            print("Invalid Book")
     with open(Areeba,'w') as f:
         json.dumps(Areeba)
 
 
-def Read():
+def Display():
     with open (Areeba,'r') as f:
         temp = json.load(f)
         i =1
@@ -57,58 +57,51 @@ def Read():
 def Edit():
     with open (Areeba,'r') as f:
         temp = json.load(f)
-    print("Enter The Book Title of The Product Which You Want To Update: ")
-    UT = input()
-     
-    if UT in Areeba.keys():
-        print()
-        q = int(input("Want to update the whole Book data Enter '0' else '1' for specific information : "))
+    UT = input("Enter The Book Title of the book You Want To edit: ")
+
+    while True: 
+        if UT in Areeba.keys():
+            print()
+            b = int(input("Want to update the all of the Books data Enter '0' else '1' for specific information : "))
          
-        if (q == 0):
+            if (b == 0):
             
-            Title = input("Enter Book Title: ")
-            Author = input("Enter the Author of the book: ")
-            BookId = input("Enter the book ID number (must be 8 Digits): ") #WHILE LOOPP
-            Genre = input("Enter the Genre of book: ")
-            Areeba[UT] = {'Title': Title, 'Author': Author,'BookId': BookId, 'Genre': Genre,}
-            
-            z = int(input("Please Press '0' to Add more Attributes/Properties of Product or Press '1' to Continue: "))
-             
-            if(z == 0):
-                print("Enter Number of New Attributes/Properties of Product :- ")
-                n = int(input())
-                for i in range(n):
-                    nam = input("Enter Attribute Name That you Want To Add :- ")
-                    pro = input("Enter The "+str(nam)+" of Product :- ")
-                    Areeba[UT][nam] = pro
-            print("The Book "+str(UT)+" is Updated Successfully")
-             
-        elif(q == 1):
-            print("Enter Which Attribute of Product You want to Update :- ")
-            p = input()
-             
-            if p in Areeba[UT].keys():
-                u = input("Enter "+str(p)+" of Product :- ")
-                Areeba[UT][p] = u
-                print("Product ID "+str(UT)+"'s attribute " +
-                      str(p)+" is Updated Successfully...!!!")
+                Title = input("Enter Book Title: ")
+                Author = input("Enter the Author of the book: ")
+                BookId = input("Enter the book ID number: ")
+                Genre = input("Enter the Genre of book: ")
+                Areeba[UT] = {'Title': Title, 'Author': Author,'BookId': BookId, 'Genre': Genre,}
+                print("The Book "+str(UT)+" is Updated Successfully")
+                break
+
+              
+            elif(b == 1):
+                d = input("Enter Which information of the Book You want to Update: ")
+
+                while True:
+                    if d in Areeba[UT].keys():
+                       u = input("Enter "+str(d)+" of book : ")
+                       Areeba[UT][d] = u
+                       print("Product ID "+str(UT)+"'s attribute " + str(d)+" is Updated Successfully...!!!")
+                       break
+                    else:
+                        print("wrong book Attribute")
+                break        
             else:
-                print("Invalid Product Attribute...!!!")
+                print("wrong option chosen")
         else:
-            print("Invalid Choice...!!!")
-    else:
-        print("Invalid Product ID...!!!")
-    json.dumps(Areeba)
+            print("Can't find Book in library ")
+        json.dumps(Areeba)
 
 
 print("Welcome to the Library!" + '\n' + "What would you like to do?" )
 
 while True:
     NN = (1,2,3,4)
-    MC = int(input( '\n' + "1. Search up a Book " + '\n' + "2. Add a new book " + '\n' + "3. Delete a book " + '\n' + "4. Edit a books information"))
+    MC = int(input( '\n' + "1. Display current library " + '\n' + "2. Add a new book " + '\n' + "3. Delete a information " + '\n' + "4. Edit a books information"))
     
     if MC == 1:
-        Read()
+        Display()
         break
     elif MC == 2:
         Create()
